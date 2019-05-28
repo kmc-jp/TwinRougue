228e6366c9133722fc6a7e68586289bc15d244ef
poketyuu <poketyuu.1215@icloud.com>
2019-05-29 04:36:32 +0900

    とりあえず動かす

--- Twin_Rougue/Assets/Scripts/PlayerMover.cs
+++ Twin_Rougue/Assets/Scripts/PlayerMover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int PlayerType;
+    public int HP;
     void Start()
     {
         
@@ -13,6 +15,17 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            this.transform.position += new Vector3(0,1,0);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            this.transform.position += new Vector3(-1,0,0);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            this.transform.position += new Vector3(0,-1,0);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            this.transform.position += new Vector3(1,0,0);
+        }
     }
 }
